c804d6123706b9cec4f8b1aca1a7c26c0013baed
helenabecker <113567226+helenabecker@users.noreply.github.com>
2024-04-19 17:50:07 -0300

    aaa

--- scripts.js
+++ scripts.js
@@ -1,21 +1,19 @@
 //Filme//
-function salvarFilmeNome(){
-    sessionStorage.setItem('filmenome', document.getElementsByClassName('filmenome').value);
-}
-
 function salvarFilme(){
-    sessionStorage.setItem('horario', document.getElementsByClassName('horario').value);
-    sessionStorage.setItem('sala', document.getElementsByClassName('sala').value);    
+    sessionStorage.setItem('filmenome', document.getElementById('filmenome').textContent);
+    sessionStorage.setItem('horario', document.getElementById('horario').textContent);
+    sessionStorage.setItem('sala', document.getElementById('sala').value);    
     }
 //Assento//
-var assento = document.querySelectorAll('input[type=checkbox]:checked'.valueOf)
+var assento;
 function salvarAssento(){
+    assento = document.querySelectorAll('input[type=checkbox]:checked'.valueOf);
     sessionStorage.setItem('assento', assento);   
 } 
     
 //Comida//
 function salvarComida(){
-    sessionStorage.setItem('comida', document.getElementsByClassName('comida').value);    
+    sessionStorage.setItem('comida', document.getElementById('comida').value);    
     }
 //Pagamento//  
 function salvarPagamento() {  
@@ -27,13 +25,13 @@ function salvarPagamento() {
 
 function carregarItensDeConfirmacao(){
 
-    document.getElementsByClassName('filme').textContent = sessionStorage.getItem('filme');
-    document.querySelectorAll('input[type=checkbox]:checked').textContent = sessionStorage.getItem('assento');
-    document.getElementsByClassName('comida').textContent = 'R$' + sessionStorage.getItem('comida');
+    document.getElementById('filmenome').textContent = sessionStorage.getItem('filmenome');
+    document.querySelectorAll('assento').value = sessionStorage.getItem('assento');
+    document.getElementById('comida').textContent = 'R$' + sessionStorage.getItem('comida');
 
     document.getElementById('nome-cartao').textContent = sessionStorage.getItem('nome-cartao');
     document.getElementById('numero-cartao').textContent = sessionStorage.getItem('numero-cartao');
-    document.getElementById('validade').textContent = 'R$' + sessionStorage.getItem('validade');
+    document.getElementById('validade').textContent = sessionStorage.getItem('validade');
     document.getElementById('cvv').textContent = sessionStorage.getItem('cvv');
 
 }
